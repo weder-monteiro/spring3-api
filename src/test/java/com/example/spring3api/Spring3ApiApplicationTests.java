a2addc0849bce34ddce623f89b22bd11f50c0feb
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-11-14 08:46:31 -0300

    first commit

---
+++ src/test/java/com/example/spring3api/Spring3ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.spring3api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring3ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
