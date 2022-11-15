fe2897327135314b099f8978c0cc8622230627db
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-11-15 19:22:17 -0300

    Ordenação

--- src/main/java/com/example/spring3api/controller/MedicoController.java
+++ src/main/java/com/example/spring3api/controller/MedicoController.java
@@ -8,6 +8,7 @@ import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
+import org.springframework.data.web.PageableDefault;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.*;
 
@@ -28,7 +29,7 @@ public class MedicoController {
     }
 
     @GetMapping
-    public Page<DadosListagemMedico> buscaTudo(Pageable paginacao) {
+    public Page<DadosListagemMedico> buscaTudo(@PageableDefault(size = 10, sort = {"nome"}) Pageable paginacao) {
         return repository.findAll(paginacao).map(DadosListagemMedico::new);
     }
 
