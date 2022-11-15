0139953c89b591d81eb8ad5b9cc1f7b2933fe45e
Weder Monteiro <weder_monteiro@outlook.com.br>
2022-11-15 17:17:37 -0300

    Validação com Bean Validation

--- src/main/java/com/example/spring3api/controller/MedicoController.java
+++ src/main/java/com/example/spring3api/controller/MedicoController.java
@@ -3,6 +3,7 @@ package com.example.spring3api.controller;
 import com.example.spring3api.medico.DadosCadastroMedico;
 import com.example.spring3api.medico.Medico;
 import com.example.spring3api.medico.MedicoRepository;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,7 +20,7 @@ public class MedicoController {
 
     @PostMapping
     @Transactional
-    public void cadastra(@RequestBody DadosCadastroMedico dados) {
+    public void cadastra(@RequestBody @Valid DadosCadastroMedico dados) {
         repository.save(new Medico(dados));
     }
 
--- src/main/java/com/example/spring3api/medico/DadosCadastroMedico.java
+++ src/main/java/com/example/spring3api/medico/DadosCadastroMedico.java
@@ -1,11 +1,24 @@
 package com.example.spring3api.medico;
 
 import com.example.spring3api.endereco.DadosEndereco;
+import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
+import jakarta.validation.constraints.Pattern;
 
 public record DadosCadastroMedico(
+        @NotBlank
         String nome,
+        @NotBlank
+        @Email
         String email,
+        @NotBlank
+        @Pattern(regexp = "\\d{4,6}")
         String crm,
+        @NotNull
         EspecialidadeEnum especialidade,
+        @NotNull
+        @Valid
         DadosEndereco endereco
 ) {}
